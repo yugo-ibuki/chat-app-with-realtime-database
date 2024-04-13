ece25628bcfb6cc42f35ff78281569f4a43178ca
yugo-ibuki <y.ibuki91@gmail.com>
2024-04-14 00:34:21 +0900

    providers ディレクトリ作成

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-import ChakraProvider from '../ChakraProvider'
+import ChakraProvider from '../providers/ChakraProvider'
 import { UserProvider } from '../contexts/LoginUserContext'
 
 const inter = Inter({ subsets: ['latin'] })
