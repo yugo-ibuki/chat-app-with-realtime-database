4a2761ef00052bbd566a3acfad049becfa06d37b
yugo-ibuki <y.ibuki91@gmail.com>
2024-04-13 22:30:26 +0900

    rooms layout作成

--- src/app/rooms/layout.tsx
+++ src/app/rooms/layout.tsx
@@ -1,16 +1,18 @@
 'use client'
 
-import { Box, Stack } from '@chakra-ui/react'
+import { Box, Button, Flex, Stack } from '@chakra-ui/react'
 import { useRouter } from 'next/navigation'
 import { useUserContext } from '../../contexts/LoginUserContext'
 import { ReactNode, useEffect } from 'react'
+import { signOut } from 'firebase/auth'
+import { auth } from '../../configs/firebase'
 
 export default function Layout({
   children,
 }: Readonly<{
   children: ReactNode
 }>) {
-  const { user, loading } = useUserContext()
+  const { user, loading, resetUser } = useUserContext()
   const router = useRouter()
 
   useEffect(() => {
@@ -27,11 +29,31 @@ export default function Layout({
     return null
   }
 
+  const handleLogout = async () => {
+    try {
+      await signOut(auth)
+      resetUser()
+      console.log('ログアウトしました')
+      router.push('/login')
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
   return (
-    <Stack spacing={8} mx="auto" maxW="lg" py={12} px={6}>
-      <Box rounded="lg" boxShadow="lg" p={8}>
-        <Stack spacing={4}>{children}</Stack>
-      </Box>
-    </Stack>
+    <>
+      <Flex alignItems="center" justifyContent="space-between" py={8} px={5}>
+        <Box>
+          user: <p>{user.email}</p>
+        </Box>
+        <Box>
+          <Button onClick={handleLogout}>ログアウト</Button>
+        </Box>
+      </Flex>
+
+      <Stack py={3} px={5}>
+        {children}
+      </Stack>
+    </>
   )
 }
--- src/app/rooms/page.tsx
+++ src/app/rooms/page.tsx
@@ -1,32 +1,7 @@
 'use client'
 
-import { signOut } from 'firebase/auth'
-import { useRouter } from 'next/navigation'
-import { Button } from '@chakra-ui/react'
-import { auth } from '../../configs/firebase'
-import { useUserContext } from '../../contexts/LoginUserContext'
-
 const Page = () => {
-  const router = useRouter()
-  const { resetUser } = useUserContext()
-
-  const handleLogout = async () => {
-    try {
-      await signOut(auth)
-      resetUser()
-      console.log('ログアウトしました')
-      router.push('/login')
-    } catch (err) {
-      console.error(err)
-    }
-  }
-
-  return (
-    <main>
-      rooms page
-      <Button onClick={handleLogout}>ログアウト</Button>
-    </main>
-  )
+  return <main>rooms</main>
 }
 
 export default Page
